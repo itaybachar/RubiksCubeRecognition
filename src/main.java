0e9b27753470bb1772f445801ee71794cf457602
Itay Bachar <itay.bachar01@gmail.com>
2018-09-08 20:14:06 -0400

    new?

--- src/main.java
+++ src/main.java
@@ -4,7 +4,7 @@ import javafx.stage.Stage;
 public class main  extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception {
-
+        primaryStage.show();
     }
 
     public static void main(String[] args) {
