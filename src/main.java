72f3bbe10831cd27790575a60af147dd17f2cf24
Itay Bachar <itay.bachar01@gmail.com>
2018-09-08 20:07:37 -0400

    added opencv library and made main entry class

---
+++ src/main.java
@@ -0,0 +1,13 @@
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class main  extends Application {
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
